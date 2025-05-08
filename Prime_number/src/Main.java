744557ffac75d4d2fd5c7545d13feb5e9ef9d6ac
Muhammet Öztürk <180087422+kaiser-dot-exe@users.noreply.github.com>
2025-05-08 21:20:16 +0300

    refactor

---
+++ Prime_number/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 21;
+
+        for (int i = 2; i <= num / 2; i++) {
+
+            if (num % i == 0) {
+                System.out.println("Your number is prime number");
+                break;
+
+            }
+
+            else {
+                System.out.println("Your number is not prime number");
+                break;
+
+            }
+
+
+        }
+
+    }
+}
