02e7989a4406351111f103212176404a5130f6b4
Muhammet Öztürk <180087422+kaiser-dot-exe@users.noreply.github.com>
2025-05-09 12:05:08 +0300

    refactor

--- Prime_number/src/Main.java
+++ Prime_number/src/Main.java
@@ -1,29 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        int num = 10;
+        int num = 9;
 
+        if (num == 2)
+        {
+            System.out.println("Your number is prime number");
+        }
+        
         for (int i = 2; i <= num / 2; i++) {
-
-            if (num == 2)
-            {
-                System.out.println("Your number is prime number");
-                break;
-            }
-            else if(num % 2 ==0)
+            if (num % i == 0 && num % 3==0)
             {
                 System.out.println("Your number is not prime number");
                 break;
 
-            }
-            else if (num % i == 0)
-            {
-                System.out.println("Your number is not prime number");
-                break;
 
             }
             else
             {
-                System.out.println("Your number is prime number");
+                System.out.println("Your number is not prime number");
                 break;
             }
 
