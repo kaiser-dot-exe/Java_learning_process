9741a71d653ab12c24fcc0dfac84eaaf26b0416d
Muhammet Öztürk <180087422+kaiser-dot-exe@users.noreply.github.com>
2025-05-18 17:45:05 +0300

    refactor

--- Midterm_and_final_exam/src/Main.java
+++ Midterm_and_final_exam/src/Main.java
@@ -1,25 +1,32 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        double midterm=35,finalexam=75,average=0;
+        Scanner input = new Scanner(System.in);
+        input.useLocale(Locale.US);
 
-        if ((midterm >= 0 && midterm <=100) && (finalexam >=0 && finalexam <=100))
-        {
-            average=(midterm*40/100)+(finalexam*60/100);
-            
-            System.out.println("Your average: "+average);
+        System.out.print("Enter your midterm exam score:");
+        double midterm = input.nextDouble();
 
-            if(finalexam >=50 && average>=50)
-            {
+        System.out.print("Enter your midterm exam score:");
+        double finalexam = input.nextDouble();
+
+        double average = 0;
+
+        if ((midterm >= 0 && midterm <= 100) && (finalexam >= 0 && finalexam <= 100)) {
+            average = (midterm * 40 / 100) + (finalexam * 60 / 100);
+
+            System.out.println("Your average: " + average);
+
+            if (finalexam >= 50 && average >= 50) {
                 System.out.println("Your pass");
-            }
-            else
-            {
+            } else {
                 System.out.println("Your fail");
             }
 
 
         }
-
 
 
     }
