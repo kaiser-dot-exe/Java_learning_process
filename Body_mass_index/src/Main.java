fda8f5a53b844c58fe7e86a2d843f0bae327ceb0
Muhammet Öztürk <180087422+kaiser-dot-exe@users.noreply.github.com>
2025-05-18 17:41:14 +0300

    refactor

--- Body_mass_index/src/Main.java
+++ Body_mass_index/src/Main.java
@@ -1,41 +1,33 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        input.useLocale(Locale.US);
 
-      double length=1.75,kilo=90,bsi;
-      // length must be in metres.
+        System.out.print("Enter your length.(Must be in metres.):");
+        double length = input.nextDouble();
 
-      bsi=kilo/(length*length);
+        System.out.print("Enter your weight:");
+        double weight = input.nextDouble();
 
-      if (bsi<18.5)
-      {
-          System.out.println("Moderate Thinness");
-      }
 
-      else if(bsi>=18.5)
-      {
-          System.out.println("Normal");
-      }
+        double bsi = weight / (length * length);
 
-      else if(bsi>=25)
-      {
-          System.out.println("Overweight");
-      }
-
-      else if(bsi>=30)
-      {
-          System.out.println("Obese Class 1");
-      }
-
-      else if(bsi>=35)
-      {
-          System.out.println("Obese Class 2");
-      }
-
-      else if(bsi>=40)
-      {
-          System.out.println("Obese Class 3");
-      }
-
+        if (bsi < 18.5) {
+            System.out.println("Moderate Thinness");
+        } else if (bsi >= 18.5) {
+            System.out.println("Normal");
+        } else if (bsi >= 25) {
+            System.out.println("Overweight");
+        } else if (bsi >= 30) {
+            System.out.println("Obese Class 1");
+        } else if (bsi >= 35) {
+            System.out.println("Obese Class 2");
+        } else if (bsi >= 40) {
+            System.out.println("Obese Class 3");
+        }
 
 
     }
